4edb1e3f3c271dcc5b4c05c167d89e90530c0f9e
tirtharajdash <dashtirtharaj@gmail.com>
2025-02-06 19:48:12 +0000

    results jak2, drd2, 0 context

--- run.sh
+++ run.sh
@@ -3,9 +3,12 @@
 #python GenMol.py --protein DBH --target_size 5 --choice 1 --context True --model gpt-4o --final_k 100
 #python GenMol.py --protein DBH --target_size 5 --choice 2 --context True --model gpt-4o --final_k 100
 #python GenMol.py --protein DBH --target_size 5 --choice 3 --context True --model gpt-4o --final_k 100
+python GenMol.py --protein DBH --target_size 0 --choice 3 --context True --model gpt-4o --final_k 100
 
 #python GenMol.py --protein JAK2 --target_size 5 --choice 1 --context True --model gpt-4o --final_k 100
 #python GenMol.py --protein JAK2 --target_size 5 --choice 2 --context True --model gpt-4o --final_k 100
 #python GenMol.py --protein JAK2 --target_size 5 --choice 3 --context True --model gpt-4o --final_k 100
+#python GenMol.py --protein JAK2 --target_size 0 --choice 3 --context True --model gpt-4o --final_k 100
 
-python GenMol.py --protein DRD2 --target_size 5 --choice 3 --context True --model gpt-4o --final_k 100
+#python GenMol.py --protein DRD2 --target_size 5 --choice 3 --context True --model gpt-4o --final_k 100
+#python GenMol.py --protein DRD2 --target_size 0 --choice 3 --context True --model gpt-4o --final_k 100
